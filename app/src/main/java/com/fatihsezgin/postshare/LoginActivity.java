9365a43460279174aa0f2993e7433e7c43b1557e
Fatih Sezgin <fsezgin936@gmail.com>
2021-03-24 17:06:31 +0300

    First Commit

---
+++ app/src/main/java/com/fatihsezgin/postshare/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.fatihsezgin.postshare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}

ddccf5577c20c4e4d8e558aff2af34d4f5f88f26
Fatih Sezgin <fsezgin936@gmail.com>
2021-03-24 15:39:23 +0300

    Initial commit

---
+++ app/src/main/java/com/fatihsezgin/postshare/MainActivity.java
@@ -0,0 +1,15 @@
+package com.fatihsezgin.postshare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+    }
+}
